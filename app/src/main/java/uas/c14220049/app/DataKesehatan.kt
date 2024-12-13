ce3bbfd3e41c8c8e67aad9c44c0c2fb105a45553
Dargo78 <stefanuswilliamsudargo0103@gmail.com>
2024-12-13 08:46:13 +0700

    feat: upload data to firebase

--- app/src/main/java/uas/c14220049/app/DataKesehatan.kt
+++ app/src/main/java/uas/c14220049/app/DataKesehatan.kt
@@ -1,5 +1,9 @@
 package uas.c14220049.app
 
 
-class DataKesehatan {
-}
+data class DataKesehatan (
+    var tanggal: String? = null,
+    var bb: Int? = 0,
+    var tekanan: Int? = 0,
+    var catatan: String? = null
+)
--- app/src/main/java/uas/c14220049/app/MainActivity.kt
+++ app/src/main/java/uas/c14220049/app/MainActivity.kt
@@ -3,13 +3,17 @@ package uas.c14220049.app
 import android.content.Intent
 import android.os.Bundle
 import android.provider.ContactsContract.Data
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -20,6 +24,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var RoomDB: DataKesehatanDB
     private lateinit var adapterKesehatan: AdapterKesehatan
     private var arrKesehatan: MutableList<DataKesehatan> = mutableListOf()
+
+    private val FirebaseDB = Firebase.firestore
+
+    fun successDialog(message: String) : AlertDialog.Builder {
+        return AlertDialog.Builder(this)
+            .setMessage(message)
+            .setTitle("Success")
+    }
+
+    fun errorDialog(message: String): AlertDialog.Builder {
+        return AlertDialog.Builder(this)
+            .setMessage("Gagal menghapus data belanja")
+            .setTitle("Error")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +59,28 @@ class MainActivity : AppCompatActivity() {
         _fabAdd.setOnClickListener {
             startActivity(Intent(this, InputDataKesehatanActivity::class.java))
         }
+
+        val _fabUpload = findViewById<FloatingActionButton>(R.id.fabUpload)
+        _fabUpload.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).async {
+                val daftarKesehatan = RoomDB.dataKesehatanDAO().selectAll()
+
+                daftarKesehatan.map {
+                    val kesehatan = uas.c14220049.app.DataKesehatan(it.tanggal, it.bb, it.tekanan, it.catatan)
+                    FirebaseDB.collection("DataKesehatan")
+                        .document(it.id.toString())
+                        .set(kesehatan)
+                        .addOnSuccessListener {
+                            successDialog("Berhasil mengupload data kesehatan ke firebase")
+                                .show()
+                        }
+                        .addOnFailureListener {
+                            errorDialog("Gagal mengupload data kesehatan ke firebase")
+                                .show()
+                        }
+                }
+            }
+        }
     }
 
     override fun onStart() {
@@ -49,8 +89,6 @@ class MainActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.Main).async {
             val daftarKesehatan = RoomDB.dataKesehatanDAO().selectAll()
             adapterKesehatan.isiData(daftarKesehatan)
-
-
         }
     }
 }
